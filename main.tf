7b8c6522fa130cc47b18c2fa0d38db885f11f74e
Tarik-3 <tarikaitahmid@gmail.com>
2025-09-05 10:20:42 +0100

    Create a cluster of servers by using ASG

--- main.tf
+++ main.tf
@@ -1,35 +1,65 @@
+
 provider "aws" {
-    region = "eu-west-3"
+ 
 }
 
-resource "aws_instance" "ec2" {
-    ami = "ami-02d7ced41dff52ebc"
-    instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.sg.id]
+resource "aws_launch_template" "ec2" {
+  name_prefix = "web-"
+  image_id         = "ami-02d7ced41dff52ebc"
+  instance_type = "t2.micro"
 
-    user_data = <<-EOF
-              #!/bin/bash
-              mkdir -p /var/www
-              echo "Hello, Tarik is here again" > /var/www/index.html
-              cd /var/www
-              nohup busybox httpd -f -p ${var.server_port} &
-              EOF
-
-    user_data_replace_on_change = true
-
-    tags = {
-        Name = "first_iac"
-    }
+  user_data = base64encode(<<-EOF
+                #!/bin/bash
+                mkdir -p /var/www
+                echo "Hello, Tarik is here again" > /var/www/index.html
+                cd /var/www
+                nohup busybox httpd -f -p ${var.server_port} &
+                EOF
+  )
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_security_group" "sg" {
-    name = "my_sg"
-    
-    ingress {
-        from_port = var.server_port
-        to_port = var.server_port
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+  name = "my_sg"
 
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_vpc" "vpc" {
+    default = true
+}
+
+data "aws_subnets" "subnets" {
+
+    filter {
+        name = "vpc-id"
+        values = [data.aws_vpc.vpc.id]
     }
 }
+
+resource "aws_autoscaling_group" "asg" {
+    launch_template {
+        id = aws_launch_template.ec2.id
+    } 
+
+    min_size = 3
+    max_size = 10
+    vpc_zone_identifier = data.aws_subnets.subnets.ids
+
+    tag {
+        key = "Name"
+        value = "terr-asg-test"
+        propagate_at_launch = true
+    }
+}
+
+
--- variables.tf
+++ variables.tf
@@ -1,4 +1,5 @@
 variable "server_port" {
-    description = "This is the server's port of our server"
-    type = number
+  description = "This is the server's port of our server"
+  type        = number
+  default     = 8080
 }
